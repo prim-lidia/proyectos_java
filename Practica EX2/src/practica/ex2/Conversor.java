265dc91a623801ea86a5f79454f532a22b1a0a0e
lidia <lidia@lidia-VirtualBox>
2017-04-28 10:26:09 +0200

    Practica EX2 - Desglose tiempo POO

---
+++ Practica EX2/src/practica/ex2/Conversor.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.ex2;
+
+/**
+ *
+ * @author lidia
+ */
+public class Conversor {
+    private int total;
+    private String[] texto;
+    private int[][]tiempo;
+    
+    public Conversor (){
+        this.total=0;
+        this.texto = new String[]{"Semanas","Días","Horas","Minutos","Segundos"};
+        this.tiempo= new int[][]{{60800,86400,3600,60,1},{0,0,0,0,0}};
+    }
+    
+    public void setTotal(int segundos){
+        this.total=segundos;
+    }
+    public void setTiempo(int segundos){
+        int auxSeg= segundos;
+        for(int i= 0; i< tiempo[0].length; i++){
+            tiempo[1][i]= auxSeg/tiempo[0][i];
+            auxSeg=auxSeg%tiempo[0][i];
+        } 
+    }
+    public int getTotal(){
+        return total;
+    }
+    public int getLength(){
+        return texto.length;
+    }
+    
+    public String getTextoDesg (int i){
+        return texto[i];
+    }
+    
+    public int getTiempoDesg(int i){
+        int cantidad=tiempo[1][i];
+        return cantidad;
+    }
+}
---
+++ Practica EX2/src/practica/ex2/PracticaEX2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.ex2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lidia
+ */
+public class PracticaEX2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar objeto
+       Conversor seg1 =  new Conversor();
+       
+       Scanner read= new Scanner(System.in);
+       System.out.println("Total de segundos:");
+       seg1.setTotal(read.nextInt());
+       
+       seg1.setTiempo(seg1.getTotal());
+       
+       System.out.println("DESGLOSE DE TIEMPO: ");
+       for(int i=0; i<seg1.getLength(); i++){
+           System.out.print(seg1.getTextoDesg(i)+": ");
+           System.out.print(seg1.getTiempoDesg(i));
+           System.out.println();
+       }
+       
+    }
+    
+}
