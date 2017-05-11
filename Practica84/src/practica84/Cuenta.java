a9e189687e4750e937d95fc26262ac2f958a79a2
lidia <lidia@lidia-VirtualBox>
2017-05-11 11:56:06 +0200

    Practica 84 - Cuentas Banco POO

--- Practica84/src/practica84/Cuenta.java
+++ Practica84/src/practica84/Cuenta.java
@@ -37,7 +37,7 @@ public class Cuenta {
     public Cuenta(String nombre, String apellido1, String apellido2, String numCuenta, double tipoInt, double saldo){
         this.nombre= nombre;
         this.apellido1=apellido1;
-        this.apellido1=apellido2;
+        this.apellido2=apellido2;
         this.numCuenta=numCuenta;
         this.tipoInt=tipoInt;
         this.saldo=saldo;
