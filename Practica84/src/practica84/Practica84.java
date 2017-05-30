756528365cf1d84a2627d2cafeda1229602a4a74
lidia <lidia@lidia-VirtualBox>
2017-05-30 12:15:16 +0200

    Cuenta POO (Comentarios)

--- Practica84/src/practica84/Practica84.java
+++ Practica84/src/practica84/Practica84.java
@@ -15,17 +15,20 @@ public class Practica84 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Crear objetos
+        //Declarar variables y objetos
         Menu m = new Menu();
         Cuenta c1 = new Cuenta("Pedro","Lopez","Mato","157484213", 0.03, 2000);
         Cuenta c2 = new Cuenta("Patricia","Martinez","Caudet","157484223", 0.05, 3500);
         String cuenta, auxCuenta, opcion="0";
-        cuenta = m.pedirCuenta();
+        
+        cuenta = m.pedirCuenta(); //Pedir numero de cuenta
+        
+        //Menu
         while(! opcion.equals("7")){
             m.mostrarMenu();
             opcion=m.pedirString();
             switch(opcion){
-                case "1":
+                case "1": // Ingreso
                     if(cuenta.equals("1")){ 
                         c1.ingreso(m.pedirImporte());
                         System.out.println("Saldo final: "+c1.getSaldo());
@@ -34,7 +37,7 @@ public class Practica84 {
                         System.out.println("Saldo final: "+c2.getSaldo());
                     }
                     break;
-                case "2":
+                case "2":// Reintegro
                     if(cuenta.equals("1")){ 
                         c1.reintegro(m.pedirImporte());
                         System.out.println("Saldo final: "+c1.getSaldo());
@@ -43,7 +46,7 @@ public class Practica84 {
                         System.out.println("Saldo final: "+c2.getSaldo());
                     }
                     break;
-                case "3":
+                case "3": //Transferencia
                     if(cuenta.equals("1")){ 
                         c1.transferencia(m.pedirImporte(),c2);
                         System.out.println("Saldo final: "+c1.getSaldo());
@@ -54,14 +57,14 @@ public class Practica84 {
                         System.out.println("Saldo beneficiario: "+c1.getSaldo());
                     }
                     break;
-                case "4":
+                case "4"://Mostrar saldo
                     if(cuenta.equals("1")){
                         m.mostrarSaldo(c1);
                     }else{
                         m.mostrarSaldo(c2);
                     }
                     break;
-                case "5":
+                case "5": //Mostrar datos
                     if(cuenta.equals("1")){
                         m.mostrarDatos(c1);
                     }else{
