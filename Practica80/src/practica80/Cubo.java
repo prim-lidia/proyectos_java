2bdee36ad452b9b0a27274d5f95dcc8b1230df38
lidia <lidia@lidia-VirtualBox>
2017-06-01 11:57:36 +0200

    Cubo POO (Comentarios)

--- Practica80/src/practica80/Cubo.java
+++ Practica80/src/practica80/Cubo.java
@@ -10,22 +10,40 @@ package practica80;
  * @author lidia
  */
 public class Cubo {
-
+    /**
+     * Arista del cubo
+     */
     private float arista;
+    /**
+     * Area del cubo
+     */
     private double area;
     
-    /* Metodos Set */
+    /**
+     * Modifica la arista del cubo
+     * @param arista longitud del la arista
+     */
     public void setArista(float arista){
         this.arista=arista;
     }
+    /**
+     * Modifica el area del cubo con su calculo
+     */
     public void setArea(){
         this.area=Math.pow(arista,2)*6;
     }
     
-    /* Metodos Get */
+    /**
+     * Devuelve la arisa del cubo
+     * @return arista del cubo
+     */
     public float getArista(){
         return arista;
     }
+    /**
+     * Devuelve el area del cubo
+     * @return area del cubo
+     */
     public double getArea(){
         return area;
     }
