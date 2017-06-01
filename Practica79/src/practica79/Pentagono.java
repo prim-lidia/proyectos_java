45fbd66bf458a2c28c403597c352efd0c579dfa6
lidia <lidia@lidia-VirtualBox>
2017-06-01 11:54:57 +0200

    Pentagono POO (Comentarios)

--- Practica79/src/practica79/Pentagono.java
+++ Practica79/src/practica79/Pentagono.java
@@ -10,28 +10,58 @@ package practica79;
  * @author lidia
  */
 public class Pentagono {
+    /**
+     * Longitud del lado del pentagono
+     */
     private float lado;
+    /**
+     * Longitud del apotema del pentagono
+     */
     private float apotema;
+    /**
+     * Area del pentagono
+     */
     private double area;
     
-    /* Metodos set */
+    /**
+     * Modifica el lado del pentagono
+     * @param n longitud del lado
+     */
     public void setLado(float n){
         this.lado = n;
     }
+    /**
+     * Modifica el apotema del pentagono
+     * @param n longitud del apotema
+     */
     public void setApotema(float n){
         this.apotema = n;
     }
+    /**
+     * Modifica el area del pentagono con el calculandola
+     */
     public void setArea(){
         this.area=(5*lado*apotema)/2;
     }
     
-    /* Metodos get */
+    /**
+     * Devuelve el lado del pentagono
+     * @return longitud del lado
+     */
     public float getLado(){
         return lado;
     }
+    /**
+     * Devuelve el apotema del pentagòno
+     * @return longitud del apotema
+     */
     public float getApotema(){
         return apotema;
     }
+    /**
+     * Devuelve el area del pentagon
+     * @return area del pentagono
+     */
     public double getArea(){
         return area;
     }
