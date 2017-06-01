cdcbebd669443890006327045c016244f1267aa1
lidia <lidia@lidia-VirtualBox>
2017-06-01 12:01:09 +0200

    Esfera POO (Comentarios)

--- Practica81/src/practica81/Esfera.java
+++ Practica81/src/practica81/Esfera.java
@@ -10,28 +10,56 @@ package practica81;
  * @author lidia
  */
 public class Esfera {
+    /**
+     * Radio de la esfera
+     */
     private float radio;
+    /**
+     * Area de la esfera
+     */
     private double area;
+    /**
+     * Volumen de la esfera
+     */
     private double volumen;
     
-    /* Metodos set */
+    /**
+     * Modifica el radio de la esfera
+     * @param n logitud del radio
+     */
     public void setRadio(float n){
         this.radio=n;
     }
+    /**
+     * Calcula y modifica el area de la esfera
+     */
     public void setArea(){
         this.area=4*Math.PI*Math.pow(radio,2);
     }
+    /**
+     * Calcula y modifica el volumen de la esfera
+     */
     public void setVolumen(){
         this.volumen=4*Math.PI*Math.pow(radio,3)/3;
     }
-    
-    /* Metodos get */
+    /**
+     * Devuelve el radio de la esfera
+     * @return radio 
+     */
     public float getRadio(){
         return radio;
     }
+    /**
+     * Devuelve el area de la esfera
+     * @return area
+     */
     public double getArea(){
         return area;
     }
+    /**
+     * Devuelve el volumen de la esfera
+     * @return volumen
+     */
     public double getVolumen(){
         return volumen;
     }
